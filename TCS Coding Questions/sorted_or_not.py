9af9c7ddcd3676934df32a89aec174b2e58b1bdb
Satwik99nuts <satwikshivam94305@gmail.com>
2026-05-21 23:47:21 +0530

    nothing

--- TCS Coding Questions/sorted_or_not.py
+++ TCS Coding Questions/sorted_or_not.py
@@ -1,7 +1,23 @@
-giv_arr = list(map(int, input("Write the array: ").split()))
+# giv_arr = list(map(int, input("Write the array: ").split()))
+# n = len(giv_arr)
+# for i in range(n-1):
+#     if giv_arr[i] > giv_arr[i+1]:
+#         print("It is not a sorted array")
+#         break
+# else:
+#     print("It is a sorted array")
+
+raw = input("Write the array: ")
+
+# If user gives spaces → split normally, else split each digit
+if " " in raw:
+    giv_arr = list(map(int, raw.split()))
+else:
+    giv_arr = list(map(int, raw))  # splits each character
+
 n = len(giv_arr)
-for i in range(n-1):
-    if giv_arr[i] > giv_arr[i+1]:
+for i in range(n - 1):
+    if giv_arr[i] > giv_arr[i + 1]:
         print("It is not a sorted array")
         break
 else:
