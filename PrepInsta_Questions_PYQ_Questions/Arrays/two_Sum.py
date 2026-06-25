c92ea016d815304f38caddcce70262ec695fa33d
Satwik99nuts <satwikshivam94305@gmail.com>
2026-06-25 11:59:06 +0530

    did brutal with two sum and now optimal approach

--- PrepInsta_Questions_PYQ_Questions/Arrays/two_Sum.py
+++ PrepInsta_Questions_PYQ_Questions/Arrays/two_Sum.py
@@ -3,11 +3,22 @@
 # You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
 # Return the answer with the smaller index first.
 
+# brute force
+# def two_sum(nums:list[int],target:int):
+#     for i in range(len(nums)):
+#         for j in range(i+1,len(nums)):
+#             if nums[i]+nums[j]==target and i<j or j<i:
+#                 return i,j
+
+# optimal-->hashmap
 def two_sum(nums:list[int],target:int):
-    for i in range(len(nums)):
-        for j in range(i+1,len(nums)):
-            if nums[i]+nums[j]==target and i<j or j<i:
-                return i,j
+    seen = {}
+    for index,num in enumerate(nums):
+        diff = target-num
+        if diff in seen:
+            return [seen[diff],index]
+        seen[num]=index
+
 nums = list(map(int,input().split()))
 target = int(input("Write the target: "))
 
