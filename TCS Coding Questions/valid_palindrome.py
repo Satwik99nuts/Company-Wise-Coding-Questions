fc268cdfe76d53d28b6dc8eaf9bde33a738db0d1
Satwik99nuts <satwikshivam94305@gmail.com>
2026-06-29 07:43:48 +0530

    final question for tcs

---
+++ TCS Coding Questions/valid_palindrome.py
@@ -0,0 +1,16 @@
+def valid_pal(s:str):
+    l,r = 0,len(s)-1
+    while l<r:
+        if not s[l].isalnum():
+            l+=1
+        elif not s[r].isalnum():
+            r-=1
+        elif s[l].lower()!=s[r].lower():
+            return False
+        else:
+            l+=1
+            r-=1
+    return True
+
+s = "A man, a plan, a canal: Panama"
+print(valid_pal(s))
