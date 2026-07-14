ad8c34e356ece0b6d9e4f8555bad1f6f80a59a4f
Satwik99nuts <satwikshivam94305@gmail.com>
2026-07-14 14:18:28 +0530

    did revision of the leetcode 88

---
+++ LeetCode_150/LC_88.py
@@ -0,0 +1,30 @@
+# # Merge Sorted Array
+
+# Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+# Output: [1,2,2,3,5,6]
+# Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+# The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+def merge_sor_arr(nums1, nums2,m,n):
+    i = m - 1
+    j = n - 1
+    k = m + n - 1
+    
+    while(i>=0 and j>=0):
+        if nums1[i]>=nums2[j]:
+            nums1[k]= nums1[i]
+            i-=1
+        else:
+            nums1[k] = nums2[j]
+            j-=1
+        k-=1
+    while(j>=0):
+        nums1[k] = nums2[j]
+        j-=1
+        k-=1
+    return nums1
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+print(merge_sor_arr(nums1,nums2,m,n))
